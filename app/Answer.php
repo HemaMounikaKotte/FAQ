040d11d43e9cfdee2d4874700a318d94d1a5815c
hema <hemamounikakotte@gmail.com>
2018-11-18 01:51:49 -0500

    Added token to view answers page

--- app/Answer.php
+++ app/Answer.php
@@ -7,7 +7,7 @@ use Illuminate\Database\Eloquent\Model;
 class answer extends Model
 {
     protected $fillable = ['body'];
-    
+
     public function user()
     {
         return $this->belongsTo('App\User');
