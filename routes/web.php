270111dd756a0ea8f01c533365bbc1062beec0cf
hema <hemamounikakotte@gmail.com>
2018-12-11 11:11:18 -0500

    Task: Add Comment model, create database (migration, factory)

--- routes/web.php
+++ routes/web.php
@@ -12,20 +12,16 @@
 Route::get('/', function () {
     return view('welcome');
 });
-
 Auth::routes();
-
 Route::get('/home', 'HomeController@index')->name('home');
-
+//Route::get('/Post', 'PostController@store')->name('Post');
 Route::get('/user/{user_id}/profile', 'ProfileController@create')->name('profile.create');
 Route::get('/user/{user_id}/profile/{profile_id}', 'ProfileController@show')->name('profile.show');
 Route::get('/user/{user_id}/profile/{profile_id}/edit', 'ProfileController@edit')->name('profile.edit');
 Route::post('/user/{user_id}/profile/', 'ProfileController@store')->name('profile.store');
 Route::patch('/user/{user_id}/profile/{profile_id}', 'ProfileController@update')->name('profile.update');
 Route::delete('/user/{user_id}/profile/{profile_id}', 'ProfileController@destroy')->name('profile.destroy');
-
 Route::get('/questions/{question_id}/answers/create', 'AnswerController@create')->name('answers.create');
-
 Route::get('/questions/{question_id}/answers/{answer_id}', 'AnswerController@show')->name('answers.show');
 Route::get('/questions/{question_id}/answers/{answer_id}/edit', 'AnswerController@edit')->name('answers.edit');
 Route::post('/questions/{question_id}/answers/', 'AnswerController@store')->name('answers.store');
@@ -34,3 +30,17 @@ Route::delete('/questions/{question_id}/answer/{answer_id}', 'AnswerController@d
 Route::resources([
     'questions' => 'QuestionController',
 ]);
+Route::post('/post/store', 'PostController@store')->middleware('auth');
+Route::get('/post/{id}', 'PostController@show')->name('post.show')->middleware('auth');
+
+Route::post('/comment/store', 'CommentController@store')->middleware('auth');
+
+Route::post('/notification/get', 'NotificationController@get');
+Route::post('/notification/read', 'NotificationController@read');
+
+
+
+//Route::get('test', function () {
+  //  event(new App\Events\StatusLiked('Someone'));
+    //return "Event has been sent!";
+//});
