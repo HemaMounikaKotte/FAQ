3b0166d12d13daa43fc3ef1fb6a57b565402f042
hema <hemamounikakotte@gmail.com>
2018-12-16 19:44:21 -0500

    unit test for user profile

--- tests/Unit/profileTest.php
+++ tests/Unit/profileTest.php
@@ -6,6 +6,7 @@ use Tests\TestCase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 
+
 class profileTest extends TestCase
 {
     /**
@@ -21,4 +22,5 @@ class profileTest extends TestCase
         $profile->user()->associate($user);
         $this->assertTrue($profile->save());
     }
+
 }
